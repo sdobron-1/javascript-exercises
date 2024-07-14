e91a8b5110cbbbbd50bd19af1a80ae2bd9a4fa3e
Sdebo1 <Steve.Dobron@hotmail.com>
2024-07-14 10:40:18 -0400

    Update exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
-};
+const sumAll = function(int1,int2) {
+    if (int1 < 0 || int2 < 0) {
+        return "ERROR";
+    } else if (typeof int1 != 'number' || typeof int2 != 'number') {
+        return "ERROR";
+    } else if (int2 > int1) {
+        let sum = 0;
+        for (i = int1; i<=int2; i++) {
+        sum = sum+=i;
+        }
+        return sum;
+    } else if (int1 > int2) {
+        let sum = 0;
+        for (i = int1; i>=int2; i--) {
+        sum = sum+=i; 
+    }
+        return sum;
+    }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
