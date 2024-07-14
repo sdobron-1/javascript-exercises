a7d2170d8e87a878f64a008b20b4a0872e23a89e
Sdebo1 <Steve.Dobron@hotmail.com>
2024-07-14 07:50:39 -0400

    Update exercises 1 through 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
-
+const repeatString = function(userInput, multiplier) {
+let functionOutput = "";
+if (multiplier<0) {
+    return "ERROR";
+}
+else {for (let i = 0; i < multiplier; i++) {
+    functionOutput+=userInput;
+}
+return functionOutput;
+}
+}
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(userInput) {
+let array = Array.from(userInput);
+array = array.reverse();
+return array.join("");
 };
 
 // Do not edit below this line
