03ba28127c14091d289d0097e4535ce5e60ff6e4
Sdebo1 <Steve.Dobron@hotmail.com>
2024-07-16 21:15:34 -0400

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (userText) {
+let reverseText = "";
+let punctuation = /[.,\/#!$%\^&\*;:{}=\-_`~()\s]/g;
+userText = userText.toLowerCase();
+userText = userText.replace(punctuation,"");
 
+for (let i=userText.length-1; i>=0; i--) {
+    reverseText+=userText.charAt(i);
+    }
+
+if (reverseText === userText) {
+    return true;
+} else {
+    return false;
+}
 };
 
 // Do not edit below this line
