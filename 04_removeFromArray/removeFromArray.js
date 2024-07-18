8959c9f0683703fe423d3467bd59e96843a3cc7c
Sdebo1 <Steve.Dobron@hotmail.com>
2024-07-18 19:40:18 -0400

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(inputArray, ...args) {
+    return inputArray.filter(val => !args.includes(val))
+    }
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
