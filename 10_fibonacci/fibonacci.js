d9178a2382b6c65da7287ce829215278e0d5584d
Sdebo1 <Steve.Dobron@hotmail.com>
2024-07-17 21:38:59 -0400

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(seqNumber) {
+seqNumber = Math.floor(seqNumber);
+if (seqNumber < 0) {
+    return "OOPS";
+} else if (seqNumber === 0) {
+    return seqNumber;
+} else if (seqNumber === 1) {
+    return seqNumber;
+} else {
+    return fibonacci(seqNumber - 1) + fibonacci(seqNumber - 2);
+}
+}
 
-};
 
 // Do not edit below this line
 module.exports = fibonacci;
